f160259c98b824f2c88a982dce2ef58acd56491a
Apinya Tangtermphol <apinya.ttp@gmail.com>
2020-11-18 22:12:35 +0700

    fix

--- vendor/magento/module-store/Block/Switcher.php
+++ vendor/magento/module-store/Block/Switcher.php
@@ -258,7 +258,7 @@ class Switcher extends \Magento\Framework\View\Element\Template
         $urlOnTargetStore = $store->getCurrentUrl(false);
         $data[ActionInterface::PARAM_NAME_URL_ENCODED] = $this->urlHelper->getEncodedUrl($urlOnTargetStore);
 
-        $url = $this->getUrl('stores/store/switch');
+        $url = $this->getUrl('stores/store/redirect');
 
         return $this->_postDataHelper->getPostData(
             $url,
