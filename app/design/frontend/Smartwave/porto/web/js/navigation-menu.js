6db6eec8783d7529c10218fae2ec8404db2b2a91
Apinya Tangtermphol <apinya.ttp@gmail.com>
2020-11-19 00:41:58 +0700

    fix

--- app/design/frontend/Smartwave/porto/web/js/navigation-menu.js
+++ app/design/frontend/Smartwave/porto/web/js/navigation-menu.js
@@ -327,7 +327,7 @@ define([
 
             if (this.options.responsive) {
                 mediaCheck({
-                    media: '(min-width: 1024px)',
+                    media: '(min-width: 768px)',
                     entry: $.proxy(function() {
                         this._toggleDesktopMode();
                     }, this),
