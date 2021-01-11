1268dcdd4f4b3f1bca9dec0f27df227104220c59
Apinya <apinya@harmonyx.co>
2021-01-11 20:09:09 +0700

    dd

--- vendor/magento/module-swatches/view/base/web/js/swatch-renderer.js
+++ vendor/magento/module-swatches/view/base/web/js/swatch-renderer.js
@@ -1280,8 +1280,7 @@ define([
                 }
             } else if (justAnImage && justAnImage.img) {
                 context.find('.product-image-photo').attr('src', justAnImage.img);
-                alert(11)
-                context.find('.product-image-photo').css('src', {"-moz-transform": "scale(1.2)", "-webkit-transform": "scale(1.2)", "transform": "scale(1.2)"});
+                context.find('.product-image-photo').css({"-moz-transform": "scale(1.2)", "-webkit-transform": "scale(1.2)", "transform": "scale(1.2)"});
             }
         },
 
