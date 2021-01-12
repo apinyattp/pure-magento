f566e8e87fe606ecb8c56596b0ea7cb220cebf65
Apinya <apinya@harmonyx.co>
2021-01-12 19:03:57 +0700

    try term ui button

--- app/code/Perspective/Kbankpayment/view/frontend/web/js/view/payment/method-renderer/kbankpayment-uibutton-term-method.js
+++ app/code/Perspective/Kbankpayment/view/frontend/web/js/view/payment/method-renderer/kbankpayment-uibutton-term-method.js
@@ -85,8 +85,8 @@ define(
                     s = document.querySelector('script[src^="' + d + '"]');
                 document.getElementById('Kbank_button').appendChild(s);
                 KPayment.create();
-console.log(KPayment)
-console.log(this.getMidKey())
+// console.log(KPayment)
+// console.log(this.getMidKey())
                 KPayment.setPublickey(this.getPublicKey());
                 KPayment.setAmount(orderData.amount);
                 KPayment.setRefNumber(orderData.order_increment_id);
