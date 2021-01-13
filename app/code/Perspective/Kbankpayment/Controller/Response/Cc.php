ea1d9b2e5b2480bb5cd8dd52258b45e6e954ed3e
Apinya <apinya@harmonyx.co>
2021-01-13 13:59:27 +0700

    try term ui button

--- app/code/Perspective/Kbankpayment/Controller/Response/Cc.php
+++ app/code/Perspective/Kbankpayment/Controller/Response/Cc.php
@@ -65,6 +65,7 @@ class Cc extends Action
      * @return void
      */
     public function execute() {
+        echo 11;die();
         $response = $this->_decode_response();
         print_r($response);die();
         $charge_id = $response['objectId'];
