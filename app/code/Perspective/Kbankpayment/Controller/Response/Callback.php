befe3187c572e42a8dcd3aa296a7ce64b0860527
Apinya <apinya@harmonyx.co>
2021-01-13 17:08:26 +0700

    try term ui button

--- app/code/Perspective/Kbankpayment/Controller/Response/Callback.php
+++ app/code/Perspective/Kbankpayment/Controller/Response/Callback.php
@@ -148,8 +148,7 @@ class Callback extends Action implements CsrfAwareActionInterface
         $response['sucess_url'] = self::PATH_SUCCESS;
         $resultJson = $this->resultJsonFactory->create();
         return $resultJson->setData($response);
-        echo 'complete';
-        // return $this->redirect(self::PATH_SUCCESS);
+        return $this->redirect('checkout/onepage/success');
     }
     public function execute22222() {
 
