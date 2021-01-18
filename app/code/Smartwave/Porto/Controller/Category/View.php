b216921a3cc12621f6b9ee554c0986e13e6304c6
Apinya <apinya@harmonyx.co>
2021-01-18 22:25:43 +0700

    dd

--- app/code/Smartwave/Porto/Controller/Category/View.php
+++ app/code/Smartwave/Porto/Controller/Category/View.php
@@ -121,7 +121,6 @@ class View extends \Magento\Catalog\Controller\Category\View
      */
     public function execute()
     {
-        echo 11;
 		if ($this->_request->getParam(\Magento\Framework\App\ActionInterface::PARAM_NAME_URL_ENCODED)) {
 			return $this->resultRedirectFactory->create()->setUrl($this->_redirect->getRedirectUrl());
 		}
