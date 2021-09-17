cab5bf67787e516a941c2d9dc3f9b8b9d3c9915a
Apinya Tanavongchinda <apinya.ttp@gmail.com>
2021-09-17 12:08:58 +0700

    test

--- app/code/Perspective/Kbankpayment/Controller/Uibutton/Charge.php
+++ app/code/Perspective/Kbankpayment/Controller/Uibutton/Charge.php
@@ -126,7 +126,9 @@ class Charge extends Action
         $payment->save();
 
         $response['sucess_url'] = self::PATH_SUCCESS;
-
+        print_r($payload);
+        print_r($response);
+        die();
         if($response['transaction_state'] == 'Authorized' && $response['status'] == 'success'){
             if($order->canInvoice()) {
                 $invoice = $this->_invoiceService->prepareInvoice($order);
