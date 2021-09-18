2c268c1046650507ea3cc44a70f9bad169dd678a
Apinya Tanavongchinda <apinya.ttp@gmail.com>
2021-09-18 12:38:20 +0700

    test

--- app/code/Perspective/Kbankpayment/Model/Config/Direct18.php
+++ app/code/Perspective/Kbankpayment/Model/Config/Direct18.php
@@ -57,8 +57,6 @@ class Direct18 extends Config
      * @return string
      */
     public function getSecret() {
-        echo '<br> isSandboxEnabled' . $this->isSandboxEnabled();
-        echo '<br> getTestSecret' . $this->getTestSecret();
         if ($this->isSandboxEnabled()) {
             return $this->getTestSecret();
         }
