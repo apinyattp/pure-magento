02710b61967da4791f078940db716813122afb24
Apinya Tanavongchinda <apinya.ttp@gmail.com>
2021-09-18 13:25:51 +0700

    test

--- app/code/Perspective/Kbankpayment/Controller/Uibutton/Charge.php
+++ app/code/Perspective/Kbankpayment/Controller/Uibutton/Charge.php
@@ -118,7 +118,7 @@ class Charge extends Action
         print_r(json_encode($payload));
         $response = $this->_makeRequest($payload);
         print_r($response);
-        exit();
+
         $response['payload'] = $payload;
         $payment->setAdditionalInformation('charge_id', $response['id']);
         $payment->setAdditionalInformation('charge_authen_url', $response['redirect_url']);
