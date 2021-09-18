6066524342d418a6b7ec8e3cd9abd64310b0d7e4
Apinya Tanavongchinda <apinya.ttp@gmail.com>
2021-09-18 15:44:03 +0700

    test payment

--- app/code/Perspective/Kbankpayment/view/frontend/web/js/view/payment/method-renderer/kbankpayment-uibutton-method.js
+++ app/code/Perspective/Kbankpayment/view/frontend/web/js/view/payment/method-renderer/kbankpayment-uibutton-method.js
@@ -78,6 +78,7 @@ define(
 
             buttonInitial: function (orderData) {
                 console.log(11, orderData)
+                console.log(22, this.getPublicKey())
                 var self = this;
                 var d = kbankURL,
                     s = document.querySelector('script[src^="' + d + '"]');
