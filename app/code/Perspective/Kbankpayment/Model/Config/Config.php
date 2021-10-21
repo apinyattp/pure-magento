fcd09f87ac6d0a4dbe1d9688b8c1ef12f74301fa
Apinya Tanavongchinda <apinya.ttp@gmail.com>
2021-10-21 23:42:25 +0700

    remove sandbox

--- app/code/Perspective/Kbankpayment/Model/Config/Config.php
+++ app/code/Perspective/Kbankpayment/Model/Config/Config.php
@@ -45,7 +45,6 @@ class Config
      * @return bool
      */
     public function isSandboxEnabled() {
-        return FALSE;
         if ($this->getValue('sandbox_status', self::CODE)) {
             return TRUE;
         }
